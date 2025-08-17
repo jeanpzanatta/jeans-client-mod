c7fffd8d920e983095745bcfc0944ecd7a0e870f
Jean Pedro Zanatta <jeanpedrozanatta@gmail.com>
2025-08-17 02:32:31 -0400

    Correção do caminho do arquivo mixin

--- src/client/java/com/jean/autofishing/mixin/client/ExampleClientMixin.java
+++ src/client/java/com/jean/autofishing/mixin/client/ExampleClientMixin.java
@@ -1,15 +1,39 @@
 package com.jean.autofishing.mixin.client;
 
+import com.jean.autofishing.AutoFishing;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.projectile.FishingBobberEntity;
+import net.minecraft.util.Hand;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftClient.class)
-public class ExampleClientMixin {
-	@Inject(at = @At("HEAD"), method = "run")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftClient.run()V
+
+/*
+Mixin para sobrescrever "onTrackedDataSet" usado em "FishingBobberEntity",
+o métod é chamado quando o peixe é fisgado, não em tod tick.
+ */
+@Mixin(FishingBobberEntity.class)
+public abstract class ExampleClientMixin {
+	@Shadow private boolean caughtFish;
+
+	@Inject(at = @At("TAIL"), method = "onTrackedDataSet")
+	public void onTrackedDataSet(TrackedData<?> data, CallbackInfo info){
+		AutoFishing.LOGGER.info("Fisgado: {}", caughtFish);
+
+		MinecraftClient cliente = MinecraftClient.getInstance();
+
+		if(caughtFish){
+			try {
+				assert cliente.interactionManager != null;
+				cliente.interactionManager.interactItem(cliente.player, Hand.MAIN_HAND);
+			} catch (NullPointerException e){
+				AutoFishing.LOGGER.info("Não foi possível pegar o peixe automaticamente");
+			}
+
+		}
 	}
 }
--- src/main/java/com/jean/autofishing/mixin/ExampleMixin.java
+++ src/main/java/com/jean/autofishing/mixin/ExampleMixin.java
@@ -1,6 +1,7 @@
 package com.jean.autofishing.mixin;
 
 import com.jean.autofishing.AutoFishing;
+import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.projectile.FishingBobberEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -10,16 +11,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 
 /*
-Mixin para sobrescrever "tick" usado em "FishingBobberEntity",
-pois a cada tick o jogo verifica se a isca foi fisgada e retorna
-um boolean. Usado apenas para teste, o console deve retornar true ou false.
+Mixin para sobrescrever "onTrackedDataSet" usado em "FishingBobberEntity",
+o métod é chamado quando o peixe é fisgado, não em tod tick.
  */
 @Mixin(FishingBobberEntity.class)
-public class ExampleMixin {
+public abstract class ExampleMixin {
 	@Shadow private boolean caughtFish;
 
-	@Inject(at = @At("TAIL"), method = "tick()V")
-	public void tick(CallbackInfo info){
+	@Inject(at = @At("TAIL"), method = "onTrackedDataSet")
+	public void onTrackedDataSet(TrackedData<?> data, CallbackInfo info){
         AutoFishing.LOGGER.info("Fisgado: {}", caughtFish);
+
 	}
 }
