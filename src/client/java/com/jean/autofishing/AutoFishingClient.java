0cb418d98845ff415eba2e909fbe6c451b6c1ee4
Jean Pedro Zanatta <jeanpedrozanatta@gmail.com>
2025-08-17 00:48:03 -0400

    Correção dos nomes de alguns arquivos.

---
+++ src/client/java/com/jean/autofishing/AutoFishingClient.java
@@ -0,0 +1,10 @@
+package com.jean.autofishing;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AutoFishingClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
