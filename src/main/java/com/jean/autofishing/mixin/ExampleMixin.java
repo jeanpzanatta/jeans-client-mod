dfce1d482c5385fb4eca66709510bd7b80e6238f
Jean Pedro Zanatta <jeanpedrozanatta@gmail.com>
2025-08-17 01:17:11 -0400

    exibindo no console quando o peixe é fisgado.

--- src/main/java/com/jean/autofishing/mixin/ExampleMixin.java
+++ src/main/java/com/jean/autofishing/mixin/ExampleMixin.java
@@ -1,15 +1,25 @@
 package com.jean.autofishing.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import com.jean.autofishing.AutoFishing;
+import net.minecraft.entity.projectile.FishingBobberEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftServer.class)
+
+/*
+Mixin para sobrescrever "tick" usado em "FishingBobberEntity",
+pois a cada tick o jogo verifica se a isca foi fisgada e retorna
+um boolean. Usado apenas para teste, o console deve retornar true ou false.
+ */
+@Mixin(FishingBobberEntity.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadWorld")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadWorld()V
+	@Shadow private boolean caughtFish;
+
+	@Inject(at = @At("TAIL"), method = "tick()V")
+	public void tick(CallbackInfo info){
+        AutoFishing.LOGGER.info("Fisgado: {}", caughtFish);
 	}
 }
